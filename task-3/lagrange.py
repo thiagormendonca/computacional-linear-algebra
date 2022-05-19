4266c8da57cfbb399fd99841fae83b9bc266b6cc
Thiago Mendonça <thiagomendonca.2801@gmail.com>
2022-05-19 19:59:05 -0300

    lagrange

---
+++ task-3/lagrange.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def lagrange(x, y, coord):
+    phi = np.array([1.] * len(y))
+
+    for i in range(len(y)):
+        for k in range(len(x)):
+            if (k == i): continue
+            phi[i] = phi[i] * ((coord - x[k])/(x[i] - x[k]))
+
+    result = 0
+
+    for i in range(len(phi)):
+        result += phi[i] * y[i]
+
+    return result
